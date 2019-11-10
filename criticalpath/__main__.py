6b23526cb7ba1a09f1b8aecde5709a7cb5d56cd1
Oliver Esser <bossadinova@gmail.com>
2019-11-10 16:08:23 +0100

    feat: use BuildstatsCache to populate graph weights

--- criticalpath/__main__.py
+++ criticalpath/__main__.py
@@ -7,11 +7,13 @@ parser = argparse.ArgumentParser(
     description='Calculate critical path in a yocto build.'
 )
 parser.add_argument('depfile', help='dependency graph in graphviz dot format')
+parser.add_argument('stats_dirs', nargs='+',
+                    help='one or more buildstats directories')
 
 
-def main(depfile):
+def main(depfile, stats_dirs):
     graph = load_graph(depfile)
-    weights = {node: 1 for node in list(graph)}
+    weights = load_weights(stats_dirs, graph)
     critical_path = criticalpath.find_critical_path(graph, weights)
 
     for node in critical_path:
@@ -23,6 +25,11 @@ def load_graph(filename):
         return criticalpath.load_graph_from_dot(dot_file)
 
 
+def load_weights(stats_dirs, graph):
+    cache = criticalpath.BuildstatsCache(stats_dirs)
+    return {node: cache.load_elapsed_time(node) for node in list(graph)}
+
+
 if __name__ == "__main__":
     args = parser.parse_args()
-    main(args.depfile)
+    main(args.depfile, args.stats_dirs)
