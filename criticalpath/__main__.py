cd4471f3122a5c1ccb6df94107b9db39ca2753f2
Oliver Esser <bossadinova@gmail.com>
2019-11-13 00:28:20 +0100

    feat: add flag to show steps with missing stats

--- criticalpath/__main__.py
+++ criticalpath/__main__.py
@@ -11,11 +11,18 @@ parser.add_argument('stats_dirs', nargs='+',
                     help='one or more buildstats directories')
 parser.add_argument('-t', '--target', help='find critical path up to target')
 parser.add_argument('-s', '--source', help='find critical path from source')
+parser.add_argument('--missing', action='store_true',
+                    help='print steps for which no stats could be found')
 
 
 def main(args):
     graph = load_graph(args.depfile)
     weights = load_weights(args.stats_dirs, graph)
+
+    if args.missing:
+        print_missing_stats(graph, weights)
+        return
+
     critical_path = criticalpath.find_critical_path(graph,
                                                     weights,
                                                     args.source,
@@ -33,6 +40,13 @@ def load_graph(filename):
 def load_weights(stats_dirs, graph):
     cache = criticalpath.BuildstatsCache(stats_dirs)
     return {node: cache.load_elapsed_time(node) for node in list(graph)}
+
+
+def print_missing_stats(graph, weights):
+    missing_stats = [node for node in list(graph)
+                     if weights[node] == 0]
+    for node in missing_stats:
+        print(node)
 
 
 if __name__ == "__main__":
